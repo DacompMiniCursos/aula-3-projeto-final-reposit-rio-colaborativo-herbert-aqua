66a66f9a5bbd55eb7ed4edbda8b559e326693594
Aqua <aqua.marinhablu@gmail.com>
2026-05-23 23:53:53 -0300

    feat: calculadora finalizada

--- calculadora.py
+++ calculadora.py
@@ -1,3 +1,8 @@
-numero1, numero2 = 0, 0
-numero1 = int(input("Digite um número: "))
-numero2 = int(input("Digite outro número: "))
+num1, num2 = 0, 0
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite outro número: "))
+def soma(num1, num2):
+    return num1 + num2
+def multiplicacao(num1, num2):
+    return num1 * num2
+print(soma(num1, num2) , multiplicacao(num1, num3))
